9b7cfd4d9f4b0f8a03327bbb483c10badea1b2de
Fabian Diaz <16745697+fdiaz79@users.noreply.github.com>
2019-05-02 01:43:21 -0400

    Updates

---
+++ precache-manifest.52d6ce413feb434c6cf70b8dcde59005.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d75dc904d60efd0c995e2a2b0d7b4e47",
+    "url": "/clickyGame/index.html"
+  },
+  {
+    "revision": "d502cded269cfab807da",
+    "url": "/clickyGame/static/css/main.158cc265.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/clickyGame/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "d502cded269cfab807da",
+    "url": "/clickyGame/static/js/main.3e185765.chunk.js"
+  },
+  {
+    "revision": "95f5ce002aae55a30c31",
+    "url": "/clickyGame/static/js/runtime~main.d5f62368.js"
+  }
+]);
