45779e751b2bdd9b3d41410b985c46d449d1bed6
Fabian Diaz <16745697+fdiaz79@users.noreply.github.com>
2019-05-02 02:01:39 -0400

    fix to public path

--- clicky/src/App.js
+++ clicky/src/App.js
@@ -15,18 +15,18 @@ class App extends Component {
         message: "Click an image to begin"
       },
     images:[
-      {id: 1, source: "%PUBLIC_URL%/images/bach.jpg", text:"Bach", clicked: false},
-      {id: 2, source: "%PUBLIC_URL%/images/beethoven.jpg", text:"Beethoven", clicked: false},
-      {id: 3, source: "%PUBLIC_URL%/images/chopin.jpg", text:"Chopin", clicked: false},
-      {id: 4, source: "%PUBLIC_URL%/images/debussy.jpg", text:"Debussy", clicked: false},
-      {id: 5, source: "%PUBLIC_URL%/images/handel.jpg", text:"Handel", clicked: false},
-      {id: 6, source: "%PUBLIC_URL%/images/mendel.jpg", text:"Mendelsohn", clicked: false},
-      {id: 7, source: "%PUBLIC_URL%/images/mozart.jpg", text:"Mozart", clicked: false},
-      {id: 8, source: "%PUBLIC_URL%/images/palestrina.jpg", text:"Palestrina", clicked: false},
-      {id: 9, source: "%PUBLIC_URL%/images/strav.jpg", text:"Stravinsky", clicked: false},
-      {id: 10, source: "%PUBLIC_URL%/images/tchai.jpg", text:"Tchaikovsky", clicked: false},
-      {id: 11, source: "%PUBLIC_URL%/images/verdi.jpg", text:"Verdi", clicked: false},
-      {id: 12, source: "%PUBLIC_URL%/images/vivaldi.jpg", text:"Vivaldi", clicked: false}
+      {id: 1, source: process.env.PUBLIC_URL+"/images/bach.jpg", text:"Bach", clicked: false},
+      {id: 2, source: process.env.PUBLIC_URL+"/images/beethoven.jpg", text:"Beethoven", clicked: false},
+      {id: 3, source: process.env.PUBLIC_URL+"/images/chopin.jpg", text:"Chopin", clicked: false},
+      {id: 4, source: process.env.PUBLIC_URL+"/images/debussy.jpg", text:"Debussy", clicked: false},
+      {id: 5, source: process.env.PUBLIC_URL+"/images/handel.jpg", text:"Handel", clicked: false},
+      {id: 6, source: process.env.PUBLIC_URL+"/images/mendel.jpg", text:"Mendelsohn", clicked: false},
+      {id: 7, source: process.env.PUBLIC_URL+"/images/mozart.jpg", text:"Mozart", clicked: false},
+      {id: 8, source: process.env.PUBLIC_URL+"/images/palestrina.jpg", text:"Palestrina", clicked: false},
+      {id: 9, source: process.env.PUBLIC_URL+"/images/strav.jpg", text:"Stravinsky", clicked: false},
+      {id: 10, source: process.env.PUBLIC_URL+"/images/tchai.jpg", text:"Tchaikovsky", clicked: false},
+      {id: 11, source: process.env.PUBLIC_URL+"/images/verdi.jpg", text:"Verdi", clicked: false},
+      {id: 12, source: process.env.PUBLIC_URL+"/images/vivaldi.jpg", text:"Vivaldi", clicked: false}
     ]
       
   }
